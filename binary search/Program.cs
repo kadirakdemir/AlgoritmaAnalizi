8476436d771d40a71d4932ca92a83110c0e12484
kadirakdemir <kadirakdemir@gmail.com>
2016-03-25 17:37:57 +0200

    Update Program.cs

--- binary search/Program.cs
+++ binary search/Program.cs
@@ -6,45 +6,50 @@ using System.Threading.Tasks;
 
 namespace odev3
 {
-    class Program
-    {
-   
-        static void Main(string[] args)
+   class Program
+     {    
+        static int hesapla(int[] dizi)
         {
-            int[] dizi = new int[2000000];
-            for (int i = 0; i < dizi.Length; i++)
-            {
-                dizi[i] = i+1;                        
-            }
-
+            Random rnd = new Random();
             int orta;
             int kucuk = 0;
             int buyuk = dizi.Length - 1;
-            int aranansayi = 62;
+            int aranansayi = rnd.Next(1,dizi.Count());
             int say = 0;
-            DateTime baslangiczamani=DateTime.Now;
-            DateTime bitiszamani = DateTime.Now;
-           TimeSpan sure;
             while (kucuk <= buyuk)
             {
                 say += 1;
                 orta = (kucuk + buyuk) / 2;
                 if (aranansayi == dizi[orta])
-                {
-                    sure = bitiszamani - baslangiczamani;
-                    Console.WriteLine("Aradığınız sayı dizinin {0} .elemanı",orta);
-                    Console.WriteLine(say);
-                    //Console.WriteLine(sure);
-                    Console.WriteLine(bitiszamani.Subtract(baslangiczamani).TotalSeconds);
-                    break;
+                {  
+                    Console.WriteLine("Aradığınız sayı dizinin {0} .elemanı  " , orta);
+                    return say;
                 }
                 else if (aranansayi < dizi[orta])
                     buyuk = orta - 1;
                 else
                     kucuk = orta + 1;
             }
-            
-            Console.ReadKey();
+            return 0;
         }
-    }
+         static void Main(string[] args)
+         {
+            int deger = 10;
+            for (int k = 0; k < 6; k++)
+            {
+                int[] dizi = new int[deger];
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    dizi[i] = i + 1;
+                }
+                DateTime baslangiczamani = DateTime.Now;
+                int say= hesapla(dizi);
+                DateTime bitiszamani = DateTime.Now;
+                Console.WriteLine(bitiszamani.Subtract(baslangiczamani));
+                Console.WriteLine(say);
+                Console.ReadKey();
+                deger = 10 * deger;
+            }
+         } 
+     } 
 }
