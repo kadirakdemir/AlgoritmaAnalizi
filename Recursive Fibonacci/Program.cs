36a8ec3328934f0b7ab7f11106764bdca3b6c5ef
kadirakdemir <kadirakdemir@gmail.com>
2016-05-06 17:56:54 +0300

    Recursive Fibonacci2

---
+++ Recursive Fibonacci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci2
+{
+    class Program
+    {  
+        public static int[] fibBuffer = new int[1000];
+        public static int fibTopDown(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else if(fibBuffer[n]!=0)
+            {
+                return fibBuffer[n];
+            }
+            else
+            {
+                fibBuffer[n] = fibTopDown(n - 1) + fibTopDown(n - 2);
+                return fibBuffer[n];
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            Console.Write("Hesaplanacak Fibonacci Sayısını Giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci Değeri  :" + fibTopDown(sayi));
+            Console.ReadKey();
+        }
+    }
+}
