4374e7a8f73a5b01a9513e6145787431afd2447a
kadirakdemir <kadirakdemir@gmail.com>
2016-05-11 16:49:43 +0300

    Char Array tersten aynı olanları bulma

--- Palindrom/Palindrom.cs
+++ Palindrom/Palindrom.cs
@@ -10,15 +10,18 @@ namespace Tersini_Bulma
     {
         static char[,] dizi;
         static Random rnd = new Random();
+		
         public static char GetLetter()
         {           
             int num = rnd.Next(0, 3); // Zero to 25
             char let = (char)('a' + num);
             return let;
         }
+		
         static public char[,] CharArrayOlustur(int n)
         {           
             dizi= new  char[n,n];
+			
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -29,13 +32,14 @@ namespace Tersini_Bulma
             return dizi;
         }
 
-        static public bool tersiniBul(int s)
+        public static bool tersiniBul(int s)
         {            
             char[,] array = new char[s,s];           
             array = CharArrayOlustur(s);
             int k = s-1;
             int yazılanlar=0;
             int sayac = 0;
+			
             for (int i = 0; i < s; i++)
             {
                 k = s-1;
@@ -58,6 +62,7 @@ namespace Tersini_Bulma
                 }
                 sayac = 0;
             }
+			
             for (int w = 0; w < s; w++)
             {
                 k = s-1;
@@ -80,17 +85,20 @@ namespace Tersini_Bulma
                 }
                 sayac = 0;
             }
+			
             if (yazılanlar==0)
             {                
-                Console.Write("Bie değer giriniz: ");
+                Console.Write("Bir değer giriniz: ");
                 Console.WriteLine(tersiniBul(Convert.ToInt32(Console.ReadLine())));
                 return false;
             }
+			
             return true;
         }
+		
         static void Main(string[] args)
         {
-            Console.Write("Bie değer giriniz: ");
+            Console.Write("Bir değer giriniz: ");
             Console.WriteLine(tersiniBul(Convert.ToInt32( Console.ReadLine())));
             Console.ReadKey();
         }
